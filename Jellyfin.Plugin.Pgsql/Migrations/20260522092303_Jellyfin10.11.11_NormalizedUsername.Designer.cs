f410c8a874f5298ac9f244145457e9a65ea757e5
JPVenson <github@jpb.email>
2026-06-11 08:56:35 +0000

    Upgrade to 10.11.11

--- Jellyfin.Plugin.Pgsql/Migrations/20260522092303_Jellyfin10.11.11_NormalizedUsername.Designer.cs
+++ Jellyfin.Plugin.Pgsql/Migrations/20260522092303_Jellyfin10.11.11_NormalizedUsername.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Jellyfin.Database.Implementations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Jellyfin.Plugin.Pgsql.Migrations
 {
     [DbContext(typeof(JellyfinDbContext))]
-    partial class JellyfinDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260522092303_AddNormalizedUsername")]
+    partial class Jellyfin101111_NormalizedUsername
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1323,9 +1326,6 @@ namespace Jellyfin.Plugin.Pgsql.Migrations
                         .HasColumnType("character varying(255)");
 
                     b.HasKey("Id");
-
-                    b.HasIndex("NormalizedUsername")
-                        .IsUnique();
 
                     b.HasIndex("Username")
                         .IsUnique();
